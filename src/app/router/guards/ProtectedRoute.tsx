06f35b06e4d734406981af937121973970f8e878
BS <bohdan.strilets@gmail.com>
2026-05-18 15:58:30 +0200

    feat(router): redirect to onboarding if onboardingCompleted is false

--- src/app/router/guards/ProtectedRoute.tsx
+++ src/app/router/guards/ProtectedRoute.tsx
@@ -3,10 +3,15 @@ import { useAuth } from '@shared/store/auth';
 import { Navigate, Outlet } from 'react-router-dom';
 
 export const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user } = useAuth();
+  const onboardingCompleted = user?.onboardingCompleted;
 
   if (!isAuthenticated) {
     return <Navigate to={ROUTES.AUTH.LOGIN} replace />;
+  }
+
+  if (user && !onboardingCompleted && location.pathname !== ROUTES.ONBOARDING) {
+    return <Navigate to={ROUTES.ONBOARDING} replace />;
   }
 
   return <Outlet />;
