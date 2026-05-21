ec1918dba05bf46dfc6923fb37433623942b635e
BS <bohdan.strilets@gmail.com>
2026-05-21 19:55:07 +0200

    feat(page): add onboarding page

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,12 +1,5 @@
-import { Box } from '@shared/ui/primitives';
 import { OnboardingStepper } from '@widgets/onboarding';
 
 export const OnboardingPage = () => {
-  return (
-    <Box p="3xl" width="full" height="full">
-      <Box width="full" style={{ maxWidth: '640px', margin: '0 auto' }}>
-        <OnboardingStepper />
-      </Box>
-    </Box>
-  );
+  return <OnboardingStepper />;
 };
