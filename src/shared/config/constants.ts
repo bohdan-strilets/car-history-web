9984d2b8e08a28e681716020522ebe61e2fe77a7
BS <bohdan.strilets@gmail.com>
2026-05-21 19:51:09 +0200

    feat(config): add constants for year and next year

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,5 +1,9 @@
 export const APP_CONSTANTS = {
   RESEND_COOLDOWN: 60,
+
   THEME_STORAGE_KEY: 'arvino-theme',
   SIDEBAR_STORAGE_KEY: 'arvino-sidebar',
+
+  CURENT_YEAR: new Date().getFullYear(),
+  NEXT_YEAR: new Date().getFullYear() + 1,
 } as const;
