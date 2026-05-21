98c56ac63599c3df4ab3f50e491778fe4c48367d
BS <bohdan.strilets@gmail.com>
2026-05-21 19:55:45 +0200

    refactor(page): remove emailVerify widget

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,9 +1,6 @@
-import { EmailVerificationBanner } from '@widgets/email-verification-banner';
-
 export const DashboardPage = () => {
   return (
     <>
-      <EmailVerificationBanner />
       <h1>DashboardPage</h1>
     </>
   );
