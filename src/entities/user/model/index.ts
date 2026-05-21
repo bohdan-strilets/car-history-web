b146e2d45fc680f524693686250bbfe4cf7992ee
BS <bohdan.strilets@gmail.com>
2026-05-21 19:33:27 +0200

    refactor(user): renamed constants

--- src/entities/user/model/index.ts
+++ src/entities/user/model/index.ts
@@ -1,2 +1,3 @@
 export { UserConstraints } from './user.constraints';
+export { USER_STATUS, type UserStatus } from './user.types';
 export type { User } from './user.types';
--- src/entities/user/model/user.types.ts
+++ src/entities/user/model/user.types.ts
@@ -1,12 +1,12 @@
 // Constants
 
-export const UserStatus = {
+export const USER_STATUS = {
   ACTIVE: 'ACTIVE',
   SUSPENDED: 'SUSPENDED',
   DELETED: 'DELETED',
 } as const;
 
-export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus];
+export type UserStatus = (typeof USER_STATUS)[keyof typeof USER_STATUS];
 
 // Types
 
