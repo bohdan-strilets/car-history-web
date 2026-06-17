e0b71faa2eabbecb2a01d90918ea08ba065c815b
BS <bohdan.strilets@gmail.com>
2026-06-17 17:05:20 +0200

    refactor(tabs): remove responsive style

--- src/shared/ui/components/tabs/Tabs.tsx
+++ src/shared/ui/components/tabs/Tabs.tsx
@@ -1,34 +1,18 @@
-import { useMediaQuery } from '@shared/hooks';
 import { ScrollView, SegmentControl } from '@shared/ui';
 
 import type { TabsProps } from './tabs.types';
 
 export const Tabs = <T extends string>({ tabs, activeTab, onChange, className }: TabsProps<T>) => {
-  const isTabletUp = useMediaQuery('tablet', 'up');
-
-  if (!isTabletUp) {
-    return (
-      <ScrollView>
-        <SegmentControl
-          value={activeTab}
-          onChange={onChange}
-          options={tabs}
-          withLabel
-          size="md"
-          className={className}
-        />
-      </ScrollView>
-    );
-  }
-
   return (
-    <SegmentControl
-      value={activeTab}
-      onChange={onChange}
-      options={tabs}
-      withLabel
-      size="md"
-      className={className}
-    />
+    <ScrollView>
+      <SegmentControl
+        value={activeTab}
+        onChange={onChange}
+        options={tabs}
+        withLabel
+        size="md"
+        className={className}
+      />
+    </ScrollView>
   );
 };
